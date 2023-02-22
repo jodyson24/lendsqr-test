e1cb498d4836b0cfb84fdf6b3ebce386047ec2f3
J Ade <joshua.adegbola20@gmail.com>
2023-02-22 11:12:27 +0100

    Adde auto margin and width

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -12,7 +12,7 @@ const initialState = {
 }
 
 type user = {
-    email: string ,
+    email: string,
     password: string | any
 }
 
@@ -36,46 +36,48 @@ const Login = () => {
 
     return (
         <>
-            <div className='page' style={{ width: auth ? '85%' : '100%'}}>
-                <Container fluid  className='px-5'>
-                    <Row className='py-5'>
-                        <Col lg={6}>
-                            <img src={logo} className="mb-5" alt="logo" />
+            <div className='page' style={{ width: auth ? '85%' : '100% !important' }}>
+                <div className='' style={{ margin: '0 auto', width: '85%'}}>
+                    <Container className='px-5'>
+                        <Row className='py-5'>
+                            <Col lg={6}>
+                                <img src={logo} className="mb-5" alt="logo" />
 
-                            <div className='mt-5'>
-                                <img src={page_img} alt="logo" />
-                            </div>
-                        </Col>
-                        <Col lg={6}>
-                            <Card className='p-5' style={{ height: '700px' }}>
-                                <Form onSubmit={handleSubmit} style={{marginTop: '105px', width: '450px'}}>
-                                    <h6 className='login-text mb-3' >Welcome!</h6>
-                                    <p className='mb-5'>Enter details to login.</p>
+                                <div className='mt-5'>
+                                    <img src={page_img} alt="logo" />
+                                </div>
+                            </Col>
+                            <Col lg={6}>
+                                <Card className='p-5' style={{ height: '700px' }}>
+                                    <Form onSubmit={handleSubmit} style={{ marginTop: '105px', width: '450px' }}>
+                                        <h6 className='login-text mb-3' >Welcome!</h6>
+                                        <p className='mb-5'>Enter details to login.</p>
 
-                                    <Col>
-                                        <Form.Group className="mb-3" controlId="formBasicEmail">
-                                            <Form.Control className='form-control-sm p-2' type="email" placeholder="Enter email" name="email" value={email} onChange={handleChange} />
-                                        </Form.Group>
-                                    </Col>
+                                        <Col>
+                                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                                <Form.Control className='form-control-sm p-2' type="email" placeholder="Enter email" name="email" value={email} onChange={handleChange} />
+                                            </Form.Group>
+                                        </Col>
 
-                                    <Col className='mb-5'>
-                                        <Form.Group className="mb-3" controlId="formBasicPassword">
-                                            <Form.Control className='form-control-sm p-2' type="password" placeholder="Password" name="password" value={password} onChange={handleChange} />
-                                        </Form.Group>
-                                    </Col>
+                                        <Col className='mb-5'>
+                                            <Form.Group className="mb-3" controlId="formBasicPassword">
+                                                <Form.Control className='form-control-sm p-2' type="password" placeholder="Password" name="password" value={password} onChange={handleChange} />
+                                            </Form.Group>
+                                        </Col>
 
-                                    <Col className='mb-3'>
-                                        <h6 className='mb-4 forgot-pass'>FORGOT PASSWORD?</h6>
-                                        <Button style={{background: '#39CDCC'}} className='btn-sm p-2 w-100' type="submit">
-                                            Submit
-                                        </Button>
-                                    </Col>
-                                </Form>
-                            </Card>
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
+                                        <Col className='mb-3'>
+                                            <h6 className='mb-4 forgot-pass'>FORGOT PASSWORD?</h6>
+                                            <Button style={{ background: '#39CDCC' }} className='btn-sm p-2 w-100' type="submit">
+                                                Submit
+                                            </Button>
+                                        </Col>
+                                    </Form>
+                                </Card>
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>
+>            </div>
         </>
     );
 }
